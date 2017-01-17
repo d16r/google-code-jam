5b52f2eed5605fc35dc63a1ef33a22f86fe5c79b
Dustin Shahidehpour <dshahidehpour@gmail.com>
2017-01-16 21:52:30 -0500

    Use data type for Votes.

--- 2017/RoundD/vote.hs
+++ 2017/RoundD/vote.hs
@@ -3,20 +3,22 @@ import System.Environment
 
 -- https://code.google.com/codejam/contest/5264486/dashboard
 
-isGoodVote :: String -> Bool
-isGoodVote "" = True
+data Vote = Yay | Nay deriving (Eq)
+
+isGoodVote :: [Vote] -> Bool
+isGoodVote [] = True
 isGoodVote s = result > 0.5
   where result = (fromIntegral good) / (fromIntegral $ length s)
-        good   = length . filter (== 'A') $ s
+        good   = length . filter (== Yay) $ s
 
-numGoodVotes :: Int -> Int -> String -> Int
+numGoodVotes :: Int -> Int -> [Vote] -> Int
 numGoodVotes 0 0 s = if (isGoodVote s) then 1 else 0
 numGoodVotes n m s = if (isGoodVote s) then (yayVotes * n) + (nayNotes * m) else 0
-  where yayVotes = if n > 0 then numGoodVotes (n-1) m $ s ++ "A" else 0
-        nayNotes = if m > 0 then numGoodVotes n (m-1) $ s ++ "B" else 0
+  where yayVotes = if n > 0 then numGoodVotes (n-1) m $ s ++ [Yay] else 0
+        nayNotes = if m > 0 then numGoodVotes n (m-1) $ s ++ [Nay] else 0
 
 calculatePercentage :: (Fractional a) => Int -> Int -> a
-calculatePercentage n m = (fromIntegral $ numGoodVotes n m "") / (fromIntegral $ factorial (n + m))
+calculatePercentage n m = (fromIntegral $ numGoodVotes n m []) / (fromIntegral $ factorial (n + m))
   where factorial 0 = 1
         factorial n = n * (factorial (n - 1))
 
