9af56bb657afb0b9d7ba6143eaa1f57d45bb5b46
Dustin Shahidehpour <dshahidehpour@gmail.com>
2017-01-16 21:48:38 -0500

    2017 Vote Problem.

---
+++ 2017/RoundD/README.md
@@ -0,0 +1,5 @@
+## To Build
+`ghc vote.hs`
+
+## To Run
+`./vote <input_file> <output_file>`
---
+++ 2017/RoundD/vote.hs
@@ -0,0 +1,35 @@
+import Data.List.Split
+import System.Environment
+
+-- https://code.google.com/codejam/contest/5264486/dashboard
+
+isGoodVote :: String -> Bool
+isGoodVote "" = True
+isGoodVote s = result > 0.5
+  where result = (fromIntegral good) / (fromIntegral $ length s)
+        good   = length . filter (== 'A') $ s
+
+numGoodVotes :: Int -> Int -> String -> Int
+numGoodVotes 0 0 s = if (isGoodVote s) then 1 else 0
+numGoodVotes n m s = if (isGoodVote s) then (yayVotes * n) + (nayNotes * m) else 0
+  where yayVotes = if n > 0 then numGoodVotes (n-1) m $ s ++ "A" else 0
+        nayNotes = if m > 0 then numGoodVotes n (m-1) $ s ++ "B" else 0
+
+calculatePercentage :: (Fractional a) => Int -> Int -> a
+calculatePercentage n m = (fromIntegral $ numGoodVotes n m "") / (fromIntegral $ factorial (n + m))
+  where factorial 0 = 1
+        factorial n = n * (factorial (n - 1))
+
+stringToInt :: String -> Int
+stringToInt s = read s :: Int
+
+main = do
+  args <- getArgs
+  content <- readFile (args !! 0)
+  let lof = map (splitOn " ") $ drop 1 $ lines content
+      results = map (\p -> calculatePercentage (stringToInt $ p !! 0) (stringToInt $ p !! 1)) lof
+      output = foldl (\out (c, v) -> out ++ "Case #" ++ show c ++ ": " ++ show v ++ "\n") ""  $ zip [1..] results 
+  writeFile (args !! 1) output
+  
+
+      
